e90db6cbcde3f9988531047b3704ad438a735a8c
Sidra-tul-muntaha <73604067+sidratulmuntahagul@users.noreply.github.com>
2020-11-25 20:04:12 +0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+var a="Assalam u alaikum"
+b="How are you"
+c="i'm almost fine"
+document.write(a);
+document.write("<br/>");
+document.write(b);
+document.write("<br/>");
+document.write(c);
+document.write("<br/>");
